eb666a388216d68f87ba3e97fa4886cacad4f8e9
Apprentice <apprentice@dbc24.local>
2013-09-05 15:36:53 -0700

    implement devise with twitter and facebook auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Storyward::Application.routes.draw do
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
---
+++ spec/models/users_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe User do
+
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:password) }
+  it { should validate_uniqueness_of(:email) }
+  it { should_not allow_value("pas").for(:password) }
+  it { should allow_value("password1").for(:password) }
+  it { should_not allow_value("example.com").for(:email) }
+  it { should allow_value("email@example.com").for(:email) }
+
+end
