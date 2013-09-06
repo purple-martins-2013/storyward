88d35865b2ae1d28569e3146d36d4df9f5bc932e
Seth Hochberg <seth@sethhochberg.com>
2013-09-05 20:52:03 -0700

    implement devise into stories controller tests

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20130906000523) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "nodes", force: true do |t|
     t.integer  "user_id"
     t.integer  "story_id"
--- spec/controllers/stories_controller_spec.rb
+++ spec/controllers/stories_controller_spec.rb
@@ -2,10 +2,26 @@ require 'spec_helper'
 
 describe StoriesController do
   render_views
+
+  let(:user){ FactoryGirl.build(:user) }
+
+  before(:each) do 
+    sign_in user
+  end
+
+  describe "#index" do
+    it "populates a list of stories" do
+      get :index
+      expect(assigns(:stories)).to eq([story])
+    end
+  end
+
   describe '#create' do
     it 'creates a story' do
       Story.should_receive(:create).with({:title => 'Sample', tail_node: 1}.with_indifferent_access)
-      post :create, user: {title: 'Sample', tail_node: 1}
+      post :create, story: {title: 'Sample', tail_node: 1}
     end
   end
+
+
 end
