3328e7a8e8bc63b219520473968208c73600095e
Apprentice <apprentice@dbc24.local>
2013-09-05 17:07:14 -0700

    create nodes_stories join table

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130905230929) do
+ActiveRecord::Schema.define(version: 20130906000523) do
 
-  create_table "stories", force: true do |t|
-    t.string  "title"
-    t.integer "tail_node"
-    t.integer "user_id"
-    t.integer "nodes_id"
-  end
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "nodes", force: true do |t|
     t.integer  "user_id"
@@ -30,6 +26,18 @@ ActiveRecord::Schema.define(version: 20130905230929) do
     t.boolean  "terminal"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "nodes_stories", force: true do |t|
+    t.integer "nodes_id"
+    t.integer "stories_id"
+  end
+
+  create_table "stories", force: true do |t|
+    t.string  "title"
+    t.integer "tail_node"
+    t.integer "user_id"
+    t.integer "nodes_id"
   end
 
   create_table "users", force: true do |t|
