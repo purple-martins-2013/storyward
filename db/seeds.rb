ba04577597fa9f2c32afdee784eedc94ea1fe06e
DBC Apprentice <apprentice@dbc.com>
2013-09-06 23:55:32 -0700

    modify seed file for appropriate testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,63 +6,67 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create(name: "First Guy", email: "test@test.com", password: "password", password_confirmation: "password")
+6.times do |index|
 
-@node = Node.create(title: "First Title", content: "First Content", parent_node: 0)
+  @user = User.create(name: "First Guy #{index}", email: "test#{index}@test.com", password: "password", password_confirmation: "password")
 
-@node.stories << Story.create(title: @node.title, user: User.first)
-@node.save
+  @node = Node.create(title: "First #{index} Title", content: "First #{index} Content", parent_node: 0)
 
-5.times do |n|
-  new_node = Node.create(title: "Second Title #{n}", content: "Second Content #{n}", parent_node: @node.id)
-  new_node.stories << Story.create(title: new_node.title, user: User.first)
-  new_node.save
-  @node.children_nodes << new_node.id
-  @node.children_nodes_will_change!
+  @node.stories << Story.create(title: @node.title, user: @user)
   @node.save
-end
 
-@node.children_nodes.each do |child|
   5.times do |n|
-    new_node = Node.create(title: "Third Title #{n} #{child}", content: "Third Content #{n} #{child}", parent_node: child)
-    new_node.stories << Story.create(title: new_node.title, user: User.first)
+    new_node = Node.create(title: "Second #{index} Title #{n}", content: "Second #{index} Content #{n}", parent_node: @node.id)
+    new_node.stories << Story.create(title: new_node.title, user: @user)
     new_node.save
-    child_node = Node.find(child)
-    child_node.children_nodes << new_node.id
-    child_node.children_nodes_will_change!
-    child_node.save
+    @node.children_nodes << new_node.id
+    @node.children_nodes_will_change!
+    @node.save
   end
-end
 
-@node.children_nodes.each do |child|
-  child_node = Node.find(child)
-  child_node.children_nodes.each do |mini_child|
+  @node.children_nodes.each do |child|
     5.times do |n|
-      new_node = Node.create(title: "Fourth Title #{n} #{child} #{mini_child}", content: "Fourth Content #{n} #{child} #{mini_child}", parent_node: mini_child)
-      new_node.stories << Story.create(title: new_node.title, user: User.first)
+      new_node = Node.create(title: "Third #{index} Title #{n} #{child}", content: "Third #{index} Content #{n} #{child}", parent_node: child)
+      new_node.stories << Story.create(title: new_node.title, user: @user)
       new_node.save
-      mini_node = Node.find(mini_child)
-      mini_node.children_nodes << new_node.id
-      mini_node.children_nodes_will_change!
-      mini_node.save
+      child_node = Node.find(child)
+      child_node.children_nodes << new_node.id
+      child_node.children_nodes_will_change!
+      child_node.save
     end
   end
-end
 
-@node.children_nodes.each do |child|
-  child_node = Node.find(child)
-  child_node.children_nodes.each do |mini_child|
-    mini_child_node = Node.find(mini_child)
-    mini_child_node.children_nodes.each do |tiny_child|
+  @node.children_nodes.each do |child|
+    child_node = Node.find(child)
+    child_node.children_nodes.each do |mini_child|
       5.times do |n|
-        new_node = Node.create(title: "Fifth Title #{n} #{child} #{mini_child} #{tiny_child}", content: "Fifth Content #{n} #{child} #{mini_child} #{tiny_child}", parent_node: tiny_child)
-        new_node.stories << Story.create(title: new_node.title, user: User.first)
+        new_node = Node.create(title: "Fourth #{index} Title #{n} #{child} #{mini_child}", content: "Fourth #{index} Content #{n} #{child} #{mini_child}", parent_node: mini_child)
+        new_node.stories << Story.create(title: new_node.title, user: @user)
         new_node.save
-        tiny_node = Node.find(tiny_child)
-        tiny_node.children_nodes << new_node.id
-        tiny_node.children_nodes_will_change!
-        tiny_node.save
+        mini_node = Node.find(mini_child)
+        mini_node.children_nodes << new_node.id
+        mini_node.children_nodes_will_change!
+        mini_node.save
       end
     end
   end
+
+  @node.children_nodes.each do |child|
+    child_node = Node.find(child)
+    child_node.children_nodes.each do |mini_child|
+      mini_child_node = Node.find(mini_child)
+      mini_child_node.children_nodes.each do |tiny_child|
+        5.times do |n|
+          new_node = Node.create(title: "Fifth #{index} Title #{n} #{child} #{mini_child} #{tiny_child}", content: "Fifth #{index} Content #{n} #{child} #{mini_child} #{tiny_child}", parent_node: tiny_child)
+          new_node.stories << Story.create(title: new_node.title, user: @user)
+          new_node.save
+          tiny_node = Node.find(tiny_child)
+          tiny_node.children_nodes << new_node.id
+          tiny_node.children_nodes_will_change!
+          tiny_node.save
+        end
+      end
+    end
+  end
+
 end
