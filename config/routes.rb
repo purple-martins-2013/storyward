ef3246ba797d975b39d88d2e5a3563c4841ee3f8
Seth Hochberg <seth@sethhochberg.com>
2013-09-06 17:52:45 -0700

    typography and homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Storyward::Application.routes.draw do
+  root "static_pages#welcome"
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :profiles, :only => [:index, :show]
   # The priority is based upon order of creation: first created -> highest priority.
