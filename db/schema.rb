453056d4a63bf782abce78eddee1ad9afd77df66
Seth Grotelueschen <sgrotelueschen@westmont.edu>
2013-09-07 19:43:02 -0700

    fix tag model typo

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130907021608) do
+ActiveRecord::Schema.define(version: 20130908023045) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,17 @@ ActiveRecord::Schema.define(version: 20130907021608) do
   create_table "stories", force: true do |t|
     t.string  "title"
     t.integer "user_id"
+  end
+
+  create_table "stories_tags", force: true do |t|
+    t.integer "story_id"
+    t.integer "tag_id"
+  end
+
+  create_table "tags", force: true do |t|
+    t.string   "word"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
