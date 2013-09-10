791023715fce275ba2c2fffe550bcdbcdfd11160
Seth Grotelueschen <sgrotelueschen@westmont.edu>
2013-09-10 11:54:25 -0700

    remove old tag specs

--- spec/models/tag_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-describe Tag do
-	it { should validate_presence_of :word }
-	it { should validate_uniqueness_of :word }
-	it { should have_and_belong_to_many :stories }
-end
