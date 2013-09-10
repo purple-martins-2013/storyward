f373fe4487ae4c82a11f9e1d664d1391d5d42587
Seth Grotelueschen <sgrotelueschen@westmont.edu>
2013-09-10 10:55:35 -0700

    remove current tag scheme

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130908232809) do
+ActiveRecord::Schema.define(version: 20130910175333) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,17 +43,6 @@ ActiveRecord::Schema.define(version: 20130908232809) do
     t.string  "title"
     t.integer "user_id"
     t.integer "node_id"
-  end
-
-  create_table "stories_tags", force: true do |t|
-    t.integer "story_id"
-    t.integer "tag_id"
-  end
-
-  create_table "tags", force: true do |t|
-    t.string   "word"
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
