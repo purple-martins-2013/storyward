53b037cf470e9413e49b75dc0bda367c93117ae4
Jeff Chang <jeffchang.ca@gmail.com>
2013-09-11 13:12:04 -0700

    remove unneeded node controller tests after removing routes

--- spec/controllers/nodes_controller_spec.rb
+++ spec/controllers/nodes_controller_spec.rb
@@ -19,51 +19,6 @@ describe NodesController do
     end
   end
 
-  describe 'GET#edit' do
-    it 'finds correct node' do
-      get :edit, :story_id => story_id, :id => node.id
-      assigns(:node).should eq(node)
-    end
-  end
-
-  describe 'PUT#update' do
-    it 'finds correct node' do
-      put :update, :id => node.id, node: (valid_params)
-      assigns(:node).should eq(node)
-    end
-
-    context 'it has children' do
-      before(:each) do
-        request.env["HTTP_REFERER"] = "where_i_came_from"
-      end
-
-      it 'redirects back with notice' do
-        Node.stub(:find).and_return(node)
-        node.stub(:children_nodes).and_return([1, 3, 4])
-
-        put :update, :story_id => story_id, :id => node.id, node: (valid_params)
-        expect(response).to redirect_to("where_i_came_from")
-        flash[:notice].should eq("Node cannot be edited because it has children.") 
-      end
-    end
-
-    context 'it has no children' do
-      before(:each) do
-        Node.stub(:find).and_return(node)
-        node.stub(:children_nodes).and_return([])
-      end
-
-      it 'updates node with params' do 
-        put :update, :story_id => story_id, :id => node.id, node: (valid_params)
-      end
-
-      it 'redirects to show path' do
-        put :update, :story_id => story_id, :id => node.id, node: (valid_params)
-        expect(response).to redirect_to node
-      end
-    end
-  end
-
   describe 'DELETE#destroy' do
     it 'finds correct node' do
       delete :destroy, :story_id => story_id, :id => node.id
