7ed32a8f310e2725bcb1dc0acfa43c700dc40376
Jeff Chang <jeffchang.ca@gmail.com>
2013-09-10 17:49:32 -0700

    add search modal and jquery for populating tags from backend

--- app/assets/javascripts/search_tags.js
+++ app/assets/javascripts/search_tags.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
 
   $('nav').on("click", "#search", function() {
-    $("#search-holder").replaceWith("<div id='search-holder' class='small-9-columns reveal-modal'><form><label for='tag_tokens'>Search by Tag:</label><input type='text' name='tag_tokens' id='tag_tokens' /><label for='search_bar'>Search by title, keyword or author:</label><input type='text' name='search_bar' id='search_bar' /><input type='submit' value='Search!' /></form><a class='close-reveal-modal'>&#215;</a></div>");
     $("#search-holder").foundation('reveal', 'open');
     populateTags();
   });
+
 });
 
 function populateTags() {
