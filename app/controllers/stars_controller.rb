e228ef58e47161050463f601e33be4131d0f88ea
Seth Grotelueschen <sgrotelueschen@westmont.edu>
2013-09-11 19:58:22 -0700

    add create and delete routes for stars

---
+++ app/controllers/stars_controller.rb
@@ -0,0 +1,13 @@
+class StarsController < ApplicationController
+
+  def create
+    Star.create(user: current_user, story_id: params[:story_id] )
+    render 'stories/_bookmark', :layout => false
+  end
+
+  def destroy
+    Star.where(user: current_user, story_id: params[:story_id] ).first.destroy
+    render 'stories/_bookmark', :layout => false
+  end
+
+end
