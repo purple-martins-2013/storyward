234c797b79f2450ab997016c24c186ae839d99ff
Jeff Chang <jeffchang.ca@gmail.com>
2013-09-13 02:15:16 +0100

    updated schema with migration

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20130911225057) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "nodes", force: true do |t|
     t.integer  "user_id"
     t.string   "title"
