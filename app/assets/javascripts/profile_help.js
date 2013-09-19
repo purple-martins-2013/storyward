503960d37dc87f8962a096033b728c3501004ba5
Seth Grotelueschen <sgrotelueschen@westmont.edu>
2013-09-19 11:52:20 -0700

    add profile page intro

---
+++ app/assets/javascripts/profile_help.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+  var introguide = introJs();
+  var startButton = $('#profile-help');
+
+  startButton.on('click', function(){
+    introguide.start();
+  });
+
+  introguide.setOptions({
+      steps: [
+          {
+            element: '#profile-picture',
+            intro: 'Welcome to Storyward! This guide will help you find your way.<br><br>Use the arrow keys for navigation or hit ESC to exit the tour immediately.',
+            position: 'bottom'
+          },
+          {
+            element: '#created-stories',
+            intro: 'Here are all the stories you\'ve created. Click the story to view it\'s tree in the \'Story Viewer\'',
+            position: 'bottom'
+          },
+          {
+            element: '.nodes-contributed',
+            intro: 'Here are nodes you\'ve added to stories. Click the title to view their tree in the \'Story Viewer\'',
+            position: 'bottom'
+          },
+          {
+            element: '#favorite-authors',
+            intro: 'Favorite authors are added when you bookmark a story. Click to view the author\'s profile.',
+            position: 'right'
+          },
+          {
+            element: '#favorite-stories',
+            intro: "Favorite stories are added when you bookmark a story. Click the title to read the story.",
+            position: 'bottom'
+          }
+      ]
+  });
+});
